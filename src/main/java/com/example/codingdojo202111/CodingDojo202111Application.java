6d13b6e6f39fa00a561f2a4e0fc9fa72e2637e1e
Tim <timklug@pm.me>
2021-11-24 11:29:22 +0100

    init

---
+++ src/main/java/com/example/codingdojo202111/CodingDojo202111Application.java
@@ -0,0 +1,13 @@
+package com.example.codingdojo202111;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingDojo202111Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodingDojo202111Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/codingdojo202111/CodingDojo202111ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.codingdojo202111;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodingDojo202111ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
