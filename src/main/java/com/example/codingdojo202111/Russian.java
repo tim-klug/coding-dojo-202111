ae0d0759b477671ca6de28554779c57f2d5ee66e
Tim <timklug@pm.me>
2021-11-24 12:09:43 +0100

    create tuple of numbers

--- src/main/java/com/example/codingdojo202111/Russian.java
+++ src/main/java/com/example/codingdojo202111/Russian.java
@@ -1,8 +1,24 @@
 package com.example.codingdojo202111;
 
+import io.vavr.Tuple;
+import io.vavr.Tuple2;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Russian {
 
   public static int Mul(int x, int y) {
     return -1;
+  }
+
+  public static List<Tuple2<Integer, Integer>> divideAndMultiply(int x, int y) {
+    var result = new ArrayList<Tuple2<Integer, Integer>>();
+    result.add(Tuple.of(x, y));
+    while(x > 1) {
+      x = x/2;
+      y = y*2;
+      result.add(Tuple.of(x, y));
+    }
+    return result;
   }
 }
--- src/test/java/com/example/codingdojo202111/MulTest.java
+++ src/test/java/com/example/codingdojo202111/MulTest.java
@@ -2,6 +2,8 @@ package com.example.codingdojo202111;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import io.vavr.Tuple;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 class MulTest {
@@ -9,5 +11,18 @@ class MulTest {
   @Test
   void callMethod() {
     assertThat(Russian.Mul(2, 4)).isNotNull();
+  }
+
+  @Test
+  void divideAndMultiplyByTwo() {
+    assertThat(Russian.divideAndMultiply(67, 52)).isEqualTo(List.of(
+        Tuple.of(67, 52),
+        Tuple.of(33, 104),
+        Tuple.of(16, 208),
+        Tuple.of(8, 416),
+        Tuple.of(4, 832),
+        Tuple.of(2, 1664),
+        Tuple.of(1, 3328)
+    ));
   }
 }
